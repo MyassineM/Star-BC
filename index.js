1695a01c9f853891e50f8f23c3da761ec2748f75
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-03-24 15:12:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -561,36 +561,9 @@ client.on('message', function(message) {//Narox
     }
 });
 
+
+	 
 client.on('message', message => {
-    var prefix = "!"
-     let command = message.content.split(" ")[0];
-   command = command.slice(prefix.length);
- 
-   let args = message.content.split(" ").slice(1);
- 
- 
- if(command == "draw") {
-     var Canvas = require('canvas')
-   , Image = new Canvas.Image
-   , canvas = new Canvas(450, 170)
-   , ctx = canvas.getContext('2d');
-   ctx.font = '30px Impact';
-   let args = message.content.split(" ").slice(1);
-   
- Image.src = canvas.toBuffer();
- 
-     console.log(Image);
- ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
- ctx.fillText(args.join("  "),110, 70);
-	 
-	 client.on("guildMemberAdd", (member) => {
-client.channels.get('559378111553339393').edit({name : `『 الأعضاء ↩ ${member.guild.memberCount} 』`});
-})
-client.on("guildMemberRemove", (member) => {
-client.channels.get('559378111553339393').edit({name : `『 الأعضاء ↩ ${member.guild.memberCount} 』`});
-})
-	 
-	 client.on('message', message => {
     if (message.content.startsWith(prefix + 'id')) {
         if (message.author.bot) return
         if (!message.guild) return message.reply('**This Command Just In Servers**')
