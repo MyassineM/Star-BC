019c19c687940081f70b6e0f2b5700cebeadd96f
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-04-07 16:29:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1288,6 +1288,34 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
-
+client.on('message', message => {
+    var prefix = ".";
+      if (!message.content.startsWith(prefix)) return;
+      var args = message.content.split(' ').slice(1);
+      var argresult = args.join(' ');
+      if (message.author.id == 410052613996937217) return;
+    
+    if (message.content.startsWith(prefix + 'p')) {
+      client.user.setGame(argresult);
+        message.channel.sendMessage(`**${argresult}** : Status changed`)
+    } else
+    
+    if (message.content.startsWith(prefix + 's')) {
+      client.user.setGame(argresult, "https://www.twitch.tv/ChampionBot");
+        message.channel.sendMessage(`**${argresult}** :The bot stream has been changed`)
+    } else
+    
+    if (message.content.startsWith(prefix + 'n')) {
+      client.user.setUsername(argresult).then
+          message.channel.sendMessage(`**${argresult}** : Name changed`)
+      return message.reply("**You**");
+    } else
+    if (message.content.startsWith(prefix + 'i')) {
+      client.user.setAvatar(argresult);
+        message.channel.sendMessage(`**${argresult}** : The bot image has been changed`);
+    
+    }
+    });
+	
 
 client.login(process.env.TOKEN);
