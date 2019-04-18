da7206f73d979f2af595de5177ae0951afb139c6
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-04-18 21:55:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2190,19 +2190,6 @@ client.on('message', message => {
  
  });
 	    
-
-
-	    
-client.on('message', function(message) {
-    if(message.content.startsWith(prefix + 'crole')) {
-        let guild = message.mentions.members.first();
-                          let ZmA = new Discord.RichEmbed()
-                  .setColor('3fcf24')
-                  .setDescription('**__✅ تم إضافة رتبة الدعم الفني لك__**')
-        message.member.addRole(message.guild.roles.find('name', 'اسم الرتبه'));
-                    message.channel.send({embed:ZmA});
-    }
-}); //Toxic Codes
 	    
 client.on('message', message => {    
     var p = "!";
